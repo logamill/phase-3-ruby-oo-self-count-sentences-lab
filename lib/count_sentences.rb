943eafd44f44682933112cec78a81927f2368cf9
Logan Miller <loganmiller214@gmail.com>
2022-05-17 16:33:59 -0400

    lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    words_array = self.split(/\.|\?|\!/)
+    filtered_words = words_array.filter do |sentence| 
+      !sentence.empty?
+    end
+    filtered_words.size
+    # self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size    
   end
 end
